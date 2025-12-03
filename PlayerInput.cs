b9f24bb8032f9848a78b9cf728ed63219cdc5388
DatOneBrainCell <crjasperdaniel@gmail.com>
2025-12-03 23:04:37 +0530

    Update PlayerInput.cs
    Removed Jump function

--- PlayerInput.cs
+++ PlayerInput.cs
@@ -1,88 +1,77 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.InputSystem.Utilities;
+using UnityEngine.InputSystem.Android;
 
 public class PlayerInput : MonoBehaviour
 {
-    [Header("Multipliers")]
-    [SerializeField] private float moveSpd; //Def = 7
-    [SerializeField] private float rotateSpd; //Def = 10
-    [SerializeField] private float jumpHeight; //Def = 5
-    [SerializeField] private float gravityMultiplier; //Def = 5
+    [SerializeField] private float movSpd;
+    [SerializeField] private float rotateSpd;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float cooldownTime;
 
-    [SerializeField] private float grndHeight;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private SphereCollider sc;
 
-    [Header("Colliders")]
-    [SerializeField] private BoxCollider grndCheck;
+    int counter = 0;
 
-    private float velocity;
-    private Collider currentCollider;
+    private bool canAtk;
+    private float atkTimer = 0;
 
-    private const string GROUND = "Ground";
+    private const string CREATURES = "Creatures";
 
     void Update()
     {
         MovePlayer();
-        Jump();
+        canAtk = Cooldown();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && canAtk) {
+            Attack();
+            atkTimer = 0;
+            counter++;
+            Debug.Log(counter);
+        }
     }
 
     private void MovePlayer() {
-        Vector2 inputVector = new Vector2(0f, 0f);
+
+        Vector2 inputVector = Vector2.zero;
 
         if(Input.GetKey(KeyCode.W)) {
             inputVector.y = +1;
         }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
+        if(Input.GetKey(KeyCode.A)) {
             inputVector.x = -1;
         }
+        if(Input.GetKey(KeyCode.S)) {
+            inputVector.y = -1;
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            inputVector.x = +1;
+        }
 
-        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpd * Time.deltaTime;
+        transform.position += moveDir * Time.deltaTime * movSpd;
 
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpd);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpd * Time.deltaTime);
     }
 
-    private bool IsGrounded() {
-        //Physics.Raycast(grndCheck.size + transform.position, Vector3.down, grndCheck.size.y / 2);
+    private void Attack() {
+        Collider[] colliderArray = Physics.OverlapSphere(sc.center, sc.radius);
 
-        Collider[] colliderArray = Physics.OverlapBox(grndCheck.center + transform.position, grndCheck.size);
+        foreach(Collider collider in colliderArray) {
+            if(collider.tag == CREATURES)
+            Debug.Log(collider.name);
+        }
+    }
 
-        foreach (Collider collider in colliderArray) {
-            if (collider.CompareTag(GROUND)) {
-                currentCollider = collider;
-                return true;
-            }
+    private bool Cooldown() {
+
+        atkTimer += Time.deltaTime;
+
+        if (atkTimer >= cooldownTime) {
+            return true;
         }
         return false;
-    }
-
-    private void Jump() {
-        Debug.Log(transform.position);
-        velocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
-
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.E)) {
-
-            velocity = Mathf.Sqrt(-2 * jumpHeight * (Physics.gravity.y * gravityMultiplier));
-
-            Vector3 surface = Physics.ClosestPoint(
-                transform.position,
-                currentCollider,
-                currentCollider.transform.position,
-                currentCollider.transform.rotation) + Vector3.up * grndHeight;
-            transform.position = new Vector3(transform.position.x, surface.y, transform.position.z);
-        }
-
-        if(velocity < 0f && IsGrounded()) {
-            velocity = 0;
-        }
-
-        transform.Translate(new Vector3(0f, velocity, 0f) * Time.deltaTime);
     }
 }
